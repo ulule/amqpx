617c611850a3e1d87d0bc665caf7e736d16df20b
Gilles Fabio <gilles@gillesfabio.com>
2018-05-31 18:33:52 +0200

    Split aliases

--- amqpx.go
+++ amqpx.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/streadway/amqp"
 )
 
 const (
@@ -16,29 +15,14 @@ const (
 	defaultRetryMaxElapsedTime  = 7 * time.Minute
 )
 
-type (
-	// Table is an alias to amqp.Table
-	Table = amqp.Table
-	// Queue is an alias to amqp.Queue
-	Queue = amqp.Queue
-	// Publishing is an alias to amqp.Publishing
-	Publishing = amqp.Publishing
-	// Delivery is an alias to amqp.Delivery
-	Delivery = amqp.Delivery
-	// Confirmation is an alias to amqp.Confirmation
-	Confirmation = amqp.Confirmation
-	// Return is an alias to amqp.Return
-	Return = amqp.Return
-	// Error is an alias to amqp.Error
-	Error = amqp.Error
-)
-
 // Client interface describe a amqp client.
 type Client interface {
 	// Channel returns a new amqp's channel from current client unless it's closed.
 	Channel() (Channel, error)
+
 	// Close closes the client.
 	Close() error
+
 	// IsClosed returns if the client is closed.
 	IsClosed() bool
 }
