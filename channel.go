2f25a8c326a9408aa94595f9fbbb1ab3601a80be
Thomas LE ROUX <thomas@leroux.io>
2018-06-11 12:58:27 +0200

    docs: More comment

--- channel.go
+++ channel.go
@@ -17,27 +17,138 @@ type Channel interface {
 	// It is safe to call this method multiple times.
 	//
 	Close() error
+	//
+	// Qos controls how many messages or how many bytes the server will try to keep on
+	// the network for consumers before receiving delivery acks. The intent of Qos is
+	// to make sure the network buffers stay full between the server and client.
+	//
 	Qos(prefetchCount int, prefetchSize int, global bool) error
+	//
+	// Cancel stops deliveries to the consumer chan established in Channel.Consume and
+	// identified by consumer.
+	//
 	Cancel(consumer string, noWait bool) error
+	//
+	// QueueDeclare declares a queue to hold messages and deliver to consumers.
+	// Declaring creates a queue if it doesn't already exist, or ensures that an
+	// existing queue matches the same parameters.
+	//
+	// Every queue declared gets a default binding to the empty exchange "" which has
+	// the type "direct" with the routing key matching the queue's name.  With this
+	// default binding, it is possible to publish messages that route directly to
+	// this queue by publishing to "" with the routing key of the queue name.
+	//
 	QueueDeclare(name string, durable bool, autoDelete bool,
 		exclusive bool, noWait bool, args Table) (Queue, error)
+	//
+	// QueueDeclarePassive is functionally and parametrically equivalent to
+	// QueueDeclare, except that it sets the "passive" attribute to true. A passive
+	// queue is assumed by RabbitMQ to already exist, and attempting to connect to a
+	// non-existent queue will cause RabbitMQ to throw an exception. This function
+	// can be used to test for the existence of a queue.
+	//
 	QueueDeclarePassive(name string, durable bool, autoDelete bool,
 		exclusive bool, noWait bool, args Table) (Queue, error)
+	//
+	// QueueInspect passively declares a queue by name to inspect the current message
+	// count and consumer count.
+	//
+	// Use this method to check how many messages ready for delivery reside in the queue,
+	// how many consumers are receiving deliveries, and whether a queue by this
+	// name already exists.
+	//
 	QueueInspect(name string) (Queue, error)
+	//
+	// QueueBind binds an exchange to a queue so that publishings to the exchange will
+	// be routed to the queue when the publishing routing key matches the binding
+	// routing key.
+	//
 	QueueBind(name string, key string, exchange string, noWait bool, args Table) error
+	//
+	// QueueUnbind removes a binding between an exchange and queue matching the key and
+	// arguments.
+	//
+	// It is possible to send and empty string for the exchange name which means to
+	// unbind the queue from the default exchange.
+	//
 	QueueUnbind(name string, key string, exchange string, args Table) error
+	//
+	// QueuePurge removes all messages from the named queue which are not waiting to
+	// be acknowledged.  Messages that have been delivered but have not yet been
+	// acknowledged will not be removed.
+	//
+	// When successful, returns the number of messages purged.
+	//
 	QueuePurge(name string, noWait bool) (int, error)
+	//
+	// QueueDelete removes the queue from the server including all bindings then
+	// purges the messages based on server configuration, returning the number of
+	// messages purged.
+	//
 	QueueDelete(name string, ifUnused bool, ifEmpty bool, noWait bool) (int, error)
+	//
+	// Consume immediately starts delivering queued messages.
+	//
+	// Begin receiving on the returned chan Delivery before any other operation on the
+	// Connection or Channel.
+	//
 	Consume(queue string, consumer string, autoAck bool, exclusive bool,
 		noLocal bool, noWait bool, args Table) (<-chan Delivery, error)
+	//
+	// ExchangeDeclare declares an exchange on the server. If the exchange does not
+	// already exist, the server will create it.  If the exchange exists, the server
+	// verifies that it is of the provided type, durability and auto-delete flags.
+	//
 	ExchangeDeclare(name string, kind string, durable bool, autoDelete bool,
 		internal bool, noWait bool, args Table) error
+	//
+	// ExchangeDeclarePassive is functionally and parametrically equivalent to
+	// ExchangeDeclare, except that it sets the "passive" attribute to true. A passive
+	// exchange is assumed by RabbitMQ to already exist, and attempting to connect to a
+	// non-existent exchange will cause RabbitMQ to throw an exception. This function
+	// can be used to detect the existence of an exchange.
+	//
 	ExchangeDeclarePassive(name string, kind string, durable bool, autoDelete bool,
 		internal bool, noWait bool, args Table) error
+	//
+	// ExchangeDelete removes the named exchange from the server. When an exchange is
+	// deleted all queue bindings on the exchange are also deleted.  If this exchange
+	// does not exist, the channel will be closed with an error.
+	//
 	ExchangeDelete(name string, ifUnused, noWait bool) error
+	//
+	// ExchangeBind binds an exchange to another exchange to create inter-exchange
+	// routing topologies on the server.  This can decouple the private topology and
+	// routing exchanges from exchanges intended solely for publishing endpoints.
+	//
+	// Binding two exchanges with identical arguments will not create duplicate
+	// bindings.
+	//
 	ExchangeBind(destination string, key string, source string, noWait bool, args Table) error
+	//
+	// ExchangeUnbind unbinds the destination exchange from the source exchange on the
+	// server by removing the routing key between them.  This is the inverse of
+	// ExchangeBind.  If the binding does not currently exist, an error will be
+	// returned.
+	//
 	ExchangeUnbind(destination string, key string, source string, noWait bool, args Table) error
+	//
+	// Publish sends a Publishing from the client to an exchange on the server.
+	//
+	// When you want a single message to be delivered to a single queue, you can
+	// publish to the default exchange with the routingKey of the queue name.  This is
+	// because every declared queue gets an implicit route to the default exchange.
+	//
 	Publish(exchange string, key string, mandatory bool, immediate bool, msg Publishing) error
+	//
+	// Get synchronously receives a single Delivery from the head of a queue from the
+	// server to the client.  In almost all cases, using Channel.Consume will be
+	// preferred.
+	//
+	// If there was a delivery waiting on the queue and that delivery was received, the
+	// second return value will be true.  If there was no delivery waiting or an error
+	// occurred, the ok bool will be false.
+	//
 	Get(queue string, autoAck bool) (msg Delivery, ok bool, err error)
 	//
 	// Ack acknowledges a delivery by its delivery tag when having been consumed with
